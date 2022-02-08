cfff3d36140c99091175affb519f5bedd2ee74cd
JacobLindsey <Jacoblindsey9121@gmail.com>
2022-02-08 13:24:33 -0600

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Good Morning! It is nice to see you!");
+        }
+    }
+}
